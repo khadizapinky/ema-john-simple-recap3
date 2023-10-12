d1464e68c2f9d685ada64d7d8ee1ccaf735f0089
khadiza pinky <khadizapinky303@gmail.com>
2023-10-12 12:21:05 +0600

    shop added

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 import Header from './Components/Header/Header'
+import Shop from './Components/Shop/Shop'
 
 function App() {
   
@@ -11,6 +12,7 @@ function App() {
   return (
     <>
       <Header></Header>
+      <Shop></Shop>
     </>
   )
 }
