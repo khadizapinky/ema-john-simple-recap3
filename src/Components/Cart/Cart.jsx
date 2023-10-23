c178321bea3fe3b4f99fe91d5626e70e4b722b32
khadiza pinky <khadizapinky303@gmail.com>
2023-10-23 09:36:41 +0600

    save cart to local storage

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -6,16 +6,18 @@ const Cart = ({cart}) => {
     console.log(cart)
     let total = 0;
     let totalShipping = 0;
+    let quantity = 0;
     for( const product of cart){
-        total = total + product.price;
+        total = total + product.price * product.quantity;
         totalShipping = totalShipping + product.shipping;
+        quantity = quantity + product.quantity;
     }
     const tax = total*7/100;
     const grandTotal = total + totalShipping + tax;
     return (
         <div>
             <h3>order summary{cart.length}</h3>
-           
+            <p>selected items: {quantity}</p>
             <p>total price:{total}</p>
             <p>total shipping : {totalShipping}</p>
             <p>tax:{tax.toFixed(2)}</p>
--- src/Components/Shop/Shop.jsx
+++ src/Components/Shop/Shop.jsx
@@ -2,38 +2,30 @@ import React, { useEffect, useState } from 'react';
 import './Shop.css';
 import Product from '../Product/Product';
 import Cart from '../Cart/Cart';
+import { addToDb, getShoppingCart } from '../../utilities/fakedb';
+
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
 
-    const [cart, setCart] = useState([]);
-
-    
-
     useEffect(()  => {
-        fetch('https://raw.githubusercontent.com/ProgrammingHero1/ema-john-resources/main/fakeData/products.json')
-        .then(res =>res.json())
-        .then(data => setProducts(data))
+           fetch('https://raw.githubusercontent.com/ProgrammingHero1/ema-john-resources/main/fakeData/products.json')
+           .then(res => res.json())
+           .then(data => setProducts(data))
     },[])
 
-    const handleAddToCart = (product) => {
-        console.log(product);
-        const newCart = [...cart, product];
-        setCart(newCart);
-    }
     return (
-        <div className='shop-container'>
-            <div className="product-container">
-                {
-                    products.map(product => <Product
-                    key={product.id}
-                    product={product}
-                    handleAddToCart={handleAddToCart}
-                    ></Product>)
-                }
-            </div>
-            <div className="cart-container">
-                   <Cart 
+                <div className='shop-container'>
+                    <div className="product-container">
+                         {
+                            products.map(product => <Product
+                                 key = {product.id}
+                                 product ={product}
+                                 ></Product>)
+                         }
+                    </div>
+         <div className="cart-container">
+                  <Cart 
                    cart={cart}
                    ></Cart>
             </div>
@@ -42,4 +34,73 @@ const Shop = () => {
     );
 };
 
+    
 export default Shop;
+
+// const Shop = () => {
+
+    // const [products, setProducts] = useState([]);
+
+    // const [cart, setCart] = useState([]);
+
+    
+
+    // useEffect(()  => {
+    //     fetch('https://raw.githubusercontent.com/ProgrammingHero1/ema-john-resources/main/fakeData/products.json')
+    //     .then(res =>res.json())
+    //     .then(data => setProducts(data))
+    // },[])
+
+    // useEffect(() => {
+    //     const storedCart = getShoppingCart();
+    //     const savedCart = [];
+    //     for(const id in storedCart){
+    //         const addedProduct = products.find(product => product.id === id);
+    //         const quantity = storedCart[id];
+    //         addedProduct.quantity = quantity
+    //         savedCart.push(addedProduct)
+    //         console.log(addedProduct);
+    //     }
+    // },[products]);
+
+    // const handleAddToCart = (product) => {
+    //     console.log(product);
+    //     let newCart = [];
+    //     const exists =cart.find(pd => pd.id === product.id);
+    //     if(!exists){
+    //         product.quantity = 1;
+    //         newCart = [...cart, product]
+    //     }
+    //     else{
+    //         exists.quantity = exists.quantity + 1;
+    //         const remaining = cart.filter(pd => pd.id !== product.id);
+    //         newCart =[...remaining, exists];
+    //     }
+
+    //     setCart(newCart);
+    //     addToDb(product.id);
+    // }
+//     return (
+//         <div className='shop-container'>
+//             <div className="product-container">
+
+
+//                 {/* {
+//                     products.map(product => <Product
+//                     key={product.id}
+//                     product={product}
+//                     handleAddToCart={handleAddToCart}
+//                     ></Product>)
+//                 } */}
+//             </div>
+//             <div className="cart-container">
+//                    <Cart 
+//                    cart={cart}
+//                    ></Cart>
+//             </div>
+            
+//         </div>
+//     );
+// };
+
+// export default Shop;
